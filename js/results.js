3c510326ee15e8c225d4619adc3ac1449bbe3825
ohsang14 <132821751+ohsang14@users.noreply.github.com>
2023-12-03 15:40:05 +0900

    Add files via upload

--- js/results.js
+++ js/results.js
@@ -1,16 +1,25 @@
 import { results, mbtis } from "./data.js";
 
-// http://127.0.0.1:5500/results.html?mbti=entp
+// http://127.0.0.1:5500/results.html?mbti=isfj
 
 const mbti = new URLSearchParams(location.search).get("mbti");
 console.log(mbti);
 const result = results[mbtis[mbti]];
 
-const titleEl = document.querySelector("page-title");
-const characterEl = document.querySelector(".character");
+const titleEl = document.querySelector(".page-title");
+const characterEL = document.querySelector(".character");
 const boxEls = document.querySelectorAll(".box");
 const jobEls = document.querySelectorAll(".job");
 const lectureEl = document.querySelector(".lecture");
 const lectureImgEl = document.querySelector(".lecture img");
 
-titleEl.innerHTML;
+titleEl.innerHTML = result.title
+characterEL.src = result.character
+boxEls.forEach(function(boxEl, index){
+    boxEl.innerHTML = result.results[index]
+})
+jobEls.forEach(function(jobEl, index) {
+    jobEl.innerHTML = result.jobs[index]
+})
+lectureEl.href = result.lectureUrl
+lectureImgEl.src = result.lectureImg
